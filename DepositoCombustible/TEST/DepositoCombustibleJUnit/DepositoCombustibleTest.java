b89743bfbb52063c2cbc2d7b543b717a57798296
FranBernabeu <fran.bernabeu88@gmail.com>
2019-01-21 22:54:22 +0100

    master

---
+++ DepositoCombustible/TEST/DepositoCombustibleJUnit/DepositoCombustibleTest.java
@@ -0,0 +1,104 @@
+package DepositoCombustibleJUnit;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class DepositoCombustibleTest {
+
+	@Test
+	public void testDepositoCombustible() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testGetDepositoNivel() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testGetDepositoMax() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testEstaVacio() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testEstaLleno() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testFill() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testConsumir() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testObject() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testGetClass() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testHashCode() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testEquals() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testClone() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testToString() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testNotify() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testNotifyAll() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testWaitLong() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testWaitLongInt() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testWait() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testFinalize() {
+		fail("Not yet implemented");
+	}
+
+}
